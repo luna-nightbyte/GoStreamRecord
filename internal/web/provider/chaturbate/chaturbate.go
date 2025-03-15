d35d25716143abc7413a28895f4f86bc42ecea7b
luna-nightbyte <lunawifu18@gmail.com>
2025-03-15 01:10:48 +0100

    removed unused function

--- internal/web/provider/chaturbate/chaturbate.go
+++ internal/web/provider/chaturbate/chaturbate.go
@@ -1,11 +1,7 @@
 package chaturbate
 
 import (
-	"GoStreamRecord/internal/db"
-	"encoding/json"
-	"log"
 	"net/http"
-	"net/url"
 	"strings"
 	"time"
 )
@@ -28,7 +24,6 @@ func (b *Chaturbate) Init(username string) any {
 // IsOnline checks if the streamer is online by checking if a thumbnail is available from the stream.
 func (c *Chaturbate) IsOnline(username string) bool {
 	// Short delay before making the call.
-
 	//Check once if a thumbnail is available
 	urlStr := "https://jpeg.live.mmcdn.com/stream?room=" + c.TrueName(username)
 	http.DefaultClient.Timeout = time.Duration(1 * time.Second)
@@ -44,41 +39,6 @@ func (c *Chaturbate) IsOnline(username string) bool {
 
 	}
 	return true
-}
-
-// Old method from recordurbate. Not really used in this app.
-// IsRoomPublic checks if a given room is public by sending a POST request.
-func (c *Chaturbate) IsRoomPublic(username string) bool {
-	// Wait for the dbured rate limit.
-	time.Sleep(time.Duration(db.Config.Settings.App.RateLimit.Time) * time.Second)
-	urlStr := "https://chaturbate.com/get_edge_hls_url_ajax/"
-	data := url.Values{}
-	data.Set("room_slug", username)
-	req, err := http.NewRequest("POST", urlStr, strings.NewReader(data.Encode()))
-	if err != nil {
-		log.Printf("Error creating request: %v", err)
-		return false
-	}
-	req.Header.Add("X-Requested-With", "XMLHttpRequest")
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Printf("Error making the request: %v", err)
-		return false
-	}
-	defer resp.Body.Close()
-
-	var res struct {
-		Success    bool   `json:"success"`
-		RoomStatus string `json:"room_status"`
-	}
-	if err := json.NewDecoder(resp.Body).Decode(&res); err != nil {
-		log.Printf("Error decoding JSON: %v", err)
-		return false
-	}
-	return res.Success && res.RoomStatus == "public"
 }
 
 // Not necessary for this as of now.
